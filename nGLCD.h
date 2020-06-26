756e20d8c7b222ef9787281a4695e567dc2aa317
nikos chalikias <nikos.chalikias@gmail.com>
2020-06-26 13:58:11 +0100

    changed from lcd to nGLCD

---
+++ nGLCD.h
@@ -0,0 +1,28 @@
+/**
+ * @brief       nGLCD.h
+ * @details    	Header file for nGLCD driver node.
+ */
+
+#ifndef __NB_nGLCD
+#define __NB_nGLCD
+
+#include "mbed.h"
+#include "nworkbench.h"
+#include "pca8561.h"
+
+class nBlock_nGLCD: public nBlockSimpleNode<1>{
+	
+	public:
+	
+	nBlock_nGLCD(PinName SDA, PinName SCL,char slave_address,char leading_zeros);
+	void triggerInput(uint32_t inputNumber, uint32_t value);
+	
+	private:
+	
+	pca8561 _nGLCD;
+	char _leading_zeros;
+		
+};
+
+
+#endif
